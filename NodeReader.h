0ac39a533e18c19f85bf6758b60b5ed20587117f
Rishub Tamirisa <rishubt2@illinois.edu>
2022-04-13 03:44:42 +0000

    graph ADT setup

--- NodeReader.h
+++ NodeReader.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <vector>
 #include <map>
+#include "graph.h"
+#include <unordered_map>
+using namespace std;
+
 class NodeReader {
     public:
-    private:
+        NodeReader(string& filename);
+        
+        void readInEdgeList(Graph& graph);
 
+        void readInLabels(string& filename);
+    private:
+        unordered_map<int, string> hash;
 };
--- graph.h
+++ graph.h
@@ -1,9 +1,30 @@
+#pragma once
 #include <vector>
 
-#pragma once
 class Graph {
     public:
-        void test();
+        class Vertex {
+            public:
+                int key;
+                Vertex(int k) : key(k) {}
+        };
+
+        class Edge {
+            public:
+                int weight;
+                Vertex* v1;
+                Vertex* v2;
+                Edge(Vertex* v1, Vertex* v2, int w) : weight(w) {}
+        };
+
+        void insertVertex(int key);
+
+        void insertEdge(Vertex* v1, Vertex* v2, int w);
+
+        std::vector<Edge> incidentEdges(Vertex* v);
+
+        bool areAdjacent(Vertex* v1, Vertex* v2);
+
     private:
         int t;
 };
