da7d71dfe5bf8c0fb0c7f3ff7c31de3aa71d7a82
Raunak Bathwal <raunak@MacBook-Pro-6.local>
2022-04-19 19:11:08 -0500

    adding py files

---
+++ modifyFile.py
@@ -0,0 +1,10 @@
+try:
+    with open("amazon-meta.txt", "r") as file_input:
+        lines = file_input.readlines()
+        with open("raunakb2-aparik31-pr16-rishubt2/225FPDataset/amazon-meta-reduced.txt", "w") as output: 
+            for line in lines:
+                if line.find("Id") != -1 or line.find("title") != -1:
+                    output.write(line)
+    print("Finished")
+except:
+    print("Error")
