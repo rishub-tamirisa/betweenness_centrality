62ec38fa00fa450bac165a83fdd695b4b1fe3591
Rishub Tamirisa <rishubt2@illinois.edu>
2022-04-20 00:46:45 +0000

    Rishub push

--- graph.cpp
+++ graph.cpp
@@ -3,7 +3,6 @@
 #include <queue>
 #include <iostream>
 //O(1)
-//test-test
 void Graph::insertVertex(int key) {
     if (adj_list.find(key) == adj_list.end()) {
         adj_list[key] = new Vertex(key);
