3ec0e8da307fff0d1da4146ee186506ab704030d
Raunak Bathwal <raunak@MacBook-Pro-6.local>
2022-04-19 19:27:17 -0500

    changes

--- modifyFile.py
+++ modifyFile.py
@@ -1,9 +1,9 @@
 try:
     with open("amazon-meta.txt", "r") as file_input: #deleted file from git because of large size
         lines = file_input.readlines()
-        with open("raunakb2-aparik31-pr16-rishubt2/225FPDataset/amazon-meta-reduced.txt", "w") as output: 
+        with open("225FPDataset/amazon-meta-reduced.txt", "w") as output: 
             for line in lines:
-                if line.find("Id") != -1 or line.find("title") != -1:
+                if line.find("Id") != -1 or line.find("discontinued") != -1 or line.find("title") != -1:
                     output.write(line)
     print("Finished")
 except:
