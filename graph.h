582a6c67984f9fae0f7e8f083534083ab9add028
Rishub Tamirisa <rishubt2@illinois.edu>
2022-05-07 00:27:44 +0000

    final code v1

--- graph.h
+++ graph.h
@@ -12,26 +12,65 @@
 #include <fstream>
 #include <stack>
 #include <cmath>
-
+/**
+* Graph class: Implemented using an adjacency list and edge list, and
+* uses Vertex and Edge private classes to manage data
+*/
 class Graph {
     public:
-        class Edge; // forward declaration
+        class Edge;
 
+        /** 
+        * Vector Class: Defines a 2D Mathematical Vector for use in Force-Directed Graph Drawing.
+        * Supports Vector Addition, Subtraction, Scalar Multiplication and Addition
+        */
         class Vector {
             public:
                 float x;
                 float y;
                 Vector(float _x, float _y) : x(_x), y(_y) {}
                 Vector() : x(0), y(0) {}
+
+                /**
+                * @name mag 
+                * Calculates the magnitude of the Vector
+                * @return magnitude of Vector
+                */
                 float mag() { return std::sqrt(x*x + y*y); }
+
+                /**
+                * @name operator+ 
+                * Adds two Vectors
+                * @return resultant Vector
+                */
                 Vector operator+(const Vector& v2) {return Vector(x + v2.x, y + v2.y); }
+               
+                /**
+                * @name operator- 
+                * Subtracts two Vectors
+                * @return resultant Vector
+                */
                 Vector operator-(const Vector& v2) {return Vector(x - v2.x, y - v2.y); }
+
+                /**
+                * @name operator/ 
+                * Divides Vector by a scalar
+                * @return resultant Vector
+                */
                 Vector operator/(const float scalar) {return Vector(x / scalar, y / scalar); }
+
+                /**
+                * @name operator*
+                * Multiplies Vector by a scalar
+                * @return resultant Vector
+                */
                 Vector operator*(const float scalar) {return Vector(x * scalar, y * scalar); }
-
-
         };
 
+        /**
+        * Vertex Class: Defines a vertex in graph theory, stores relevant attributes for functions,
+        * edge list, and degree
+        */
         class Vertex {
             public:
                 Vector pos;
@@ -41,74 +80,197 @@ class Graph {
                 int ID;
                 int degree;
                 std::list<Edge*> edges;
+
+                /**
+                * @name Vertex
+                * Define a Vector with an ID
+                */
                 Vertex(int id) : centrality(0), color(0), ID(id), degree(-1) {
                     Vector disp_temp(0,0);
                     disp = disp_temp;
                 }
         };
 
+        /**
+        * Edge Class: Defines an edge in graph theory, stores a weight and two vertices
+        */
         class Edge {
             public:
                 int weight;
                 Vertex* v1;
                 Vertex* v2;
+                
+                /**
+                * @name Edge
+                * Define an Edge with two Vertices and a weight
+                */
                 Edge(Vertex* _v1, Vertex* _v2, int w) : weight(w), v1(_v1), v2(_v2) {}
         };
-
         
 
-        
-
+        /**
+        * @name insertVertex
+        * inserts a vertex into the Graph ADT
+        * Runs in O(1)
+        * @param key the key of the vertex to be inserted
+        */
         void insertVertex(int key);
 
+        /**
+        * @name insertEdge
+        * inserts an edge into the Graph ADT
+        * @param k1 a key on one side of the edge
+        * @param k2 a key on one side of the edge
+        * @param w the weight of the edge
+        * Runs in O(max(deg(v1), deg(v2)))
+        */
         void insertEdge(int k1, int k2, int w);
 
+        /**
+        * @name incidentEdges
+        * inserts an edge into the Graph ADT
+        * @param key the vertex whose edges are being returned
+        * @return a list of Edge pointers
+        * Runs in O(1)
+        */
         std::list<Edge*> incidentEdges(int key);
 
+        /**
+        * @name areAdjacent
+        * checks if two vertices are Adjacent
+        * @param k1 the key of the first vertex to check
+        * @param k2 the key of the second vertex to chec
+        * @return a bool specifying if the input keys are adjacent
+        * Runs in O(max(deg(v1), deg(v2))) ~ O(1)
+        */
         bool areAdjacent(int k1, int k2);
 
-
-        //access vertices
-        Vertex* operator[](int key) { return adj_list.at(key); }
-
-        //BFS
+        /**
+        * @name BFS
+        * find the shortest path between two vertices
+        * @param root the key of the first vertex in the path
+        * @param target the key of the last vertex in the path
+        * @return a vector of keys following the path from root to target
+        * Runs in O(V+E)
+        */
         std::vector<int> BFS(int root, int target);
 
-        void Brandes_BFS_helper(int s, std::unordered_map<int,std::vector<int>>& pred, 
-                        std::unordered_map<int, int>& sigma, std::stack<int>& stack);
-
-
+        /**
+        * @name betweennes_centrality
+        * determine the centrality of nodes in the graph.
+        * An implementation of the Brandes Algorithm for betweenness centrality described in the 
+        * original Brandes algorithm paper (Algorithm 1)
+        * Brandes, U. 2001. A Faster Algorithm for Betweenness Centrality. Journal of Mathematical Sociology, 163--177.
+        * @param k the number of nodes to return for centrality output
+        * @return a vector of k pairs of node IDs and their centralities
+        * Runs in O(V^2)
+        */
         std::vector<std::pair<int, float>> betweenness_centrality(int k);
         
-        bool map_compare(std::pair<int, float>& a, std::pair<int, float>& b);
+        /**
+        * @name connected_subgraph
+        * find a connected subgraph within the overall graph, knowing the graph is fully connected
+        * @param root the node to generate the subgraph from
+        * @param size the number of nodes in the subgraph
+        * @return a subgraph starting from root of size: size
+        * Runs in O(V+E)
+        */
+        Graph connected_subgraph(int root, int size);
 
-        Graph connected_subgraph(int root, int size, bool write);
+        /**
+        * @name draw_graph
+        * draw the graph within a specified name, and output in SVG format to a specified file location
+        * @param loc the file location to save the SVG to
+        * @param width the width of the output frame
+        * @param height the height of the output frame
+        * @param ID bool for whether to display node IDs
+        * Runs in O(V^2)
+        */
+        void draw_graph(std::string loc, int width, int height, bool ID);
 
-        //FORCE-DIRECTED DRAWING
-
-        void draw_graph(int x, int y, bool ID);
-
-        void calc_forces(float x, float y);
-
-        void init_pos(int x, int y);
-
-        void set_dims(int& x, int& y);
-
+        /**
+        * @name ~Graph
+        * clears the memory associated with the graph
+        * Runs in O(V+E)
+        */
         ~Graph();
         
-        //testing purposes
+        /**
+        * @name getAdjList
+        * returns the adjacency list. Used only for testing purposes
+        * @return the adjacency list
+        */
         std::unordered_map<int, Vertex*>& getAdjList() { return adj_list; }
 
+        /**
+        * @name getEdgeList
+        * returns the edge list. Used only for testing purposes
+        * @return the edge list
+        */
         std::vector<Edge*>& getEdgeList() { return edge_list; }
 
-        void normalize_bc();
-
-
     private:
+        /**
+        * @name bc_computed
+        * bool that keeps track of when betweenness centrality is computed
+        */
         bool bc_computed = false;
-        std::unordered_map<int, Vertex*> adj_list;
-        std::vector<Edge*> edge_list;
 
+        /**
+        * @name adj_list
+        * a hashmap adjacency list from keys to Vertex pointers
+        */
+        std::unordered_map<int, Vertex*> adj_list;
+
+        /**
+        * @name edge_list
+        * a vector edge list of Edge pointers
+        */
+        std::vector<Edge*> edge_list;
+        
+        /**
+        * @name Brandes_BFS_helper
+        * a BFS implementation the finds all shortests paths from a specified starting node to every other node.
+        * follows the BFS algorithm described in the original Brandes algorithm paper (Algorithm 1)
+        * Brandes, U. (2001). A Faster Algorithm for Betweenness Centrality. Journal of Mathematical Sociology (p./pp. 163--177)
+        */
+        void Brandes_BFS_helper(int s, std::unordered_map<int,std::vector<int>>& pred, std::unordered_map<int, int>& sigma, std::stack<int>& stack);
+
+        /**
+        * @name calc_forces
+        * Calculates the attractive and repulsive forces on graph vertices to convergence.
+        * An altered implementation of the Fruchterman and Reingold Force Directed Graph Drawing n-body algorithm
+        * Thomas M. J. Fruchterman and Edward M. Reingold. 1991. Graph drawing by force-directed placement. Softw. Pract. 
+        *   Exper. 21, 11 (Nov. 1991), 1129–1164. https://doi.org/10.1002/spe.4380211102
+        * @param width the width of the frame
+        * @param height the height of the frame
+        */
+        void calc_forces(float width, float height);
+
+        /**
+        * @name init_pos
+        * randomly initialize Vertex position vectors within the frame
+        * @param width the width of the frame
+        * @param height the height of the frame
+        */
+        void init_pos(int width, int height);
+
+        /**
+        * @name set_dims
+        * rescales the x and y dimensions of the SVG frame in the draw_graph function to fit the
+        * maximum and minimum vertex positions found by Fruchterman Reingold. Adjusts vertex positions 
+        * to fit in bounds
+        * @param width the frame width to be modified
+        * @param height the frame height to be modified
+        */
+        void set_dims(int& width, int& height);
+        
+        /**
+        * @name normalize_bc
+        * normalize the betweenness centrality values within a reasonable range. Assigns vertex colors based 
+        * on their centralities for better graph drawing output
+        */
+        void normalize_bc();
 
 
 };
--- main.cpp
+++ main.cpp
@@ -10,15 +10,99 @@
 #include "graph.h"
 #include "NodeReader.h"
 
-int main() {
+int main(int argc, char** argv) {
+  //Example Input: ./graph "225FPDataset/com-amazon-reduced.ungraph.txt" "225FPDataset/amazon-meta-reduced.txt" "" 100000 500 10 500 500 1 404753
+  //Example Input with no Labels: ./graph "225FPDataset/com-amazon-reduced.ungraph.txt" "" "" 100000 500 10 500 500 1 404753
+
+  /*
+  * Command Line Argument Validation
+  */
+  if (argc == 2) {
+    std::string input = argv[1];
+    if (input == "help") {
+      std::cout << "Use ./graph [input loc] [input labels] [output loc] [starting node] [size] [# of BC vals] [img x dim] [img y dim] [BFS start ID] [BFS end ID]" << std::endl;
+    } else {
+      std::cout << "Use ./graph 'help' to display the input options for this executable." << std::endl;
+    }
+    return 0;
+  }
+  if (argc != 11) {
+    std::cout << "Invalid Arguments. Use ./graph [input loc] [input labels] [output loc] [starting node] [size] [# of BC vals] [img x dim] [img y dim] [BFS start ID] [BFS end ID]" << std::endl;
+    std::cout << "\nUse \"\" for [output loc] to specify the general directory as the output location" << std::endl;
+    std::cout << "\nUse \"\" for [input labels] if there are no labels" << std::endl;
+    return 0;
+  }
+  std::vector<std::string> args;
+  std::cout << "Your input: ";
+  for (int i = 0; i < argc; ++i) {
+    args.push_back(argv[i]);
+    std::cout << args[i] << " ";
+  }
+  std::cout << "\n";
+
+  /*
+  * Read in Graph
+  */
   Graph graph;
-  NodeReader reader("225FPDataset/com-amazon-reduced.ungraph.txt");
+  NodeReader reader(args[1]);
   reader.readInEdgeList(graph);
-  Graph sub = graph.connected_subgraph(100000, 750, true);
-  // NodeReader reader("connected_size_300.txt");
-  // reader.readInEdgeList(graph);
-  sub.betweenness_centrality(10);
-  sub.draw_graph(1000, 1000, true);
+  reader.readInLabels(args[2]);
+
+  Graph sub = graph.connected_subgraph(std::stoi(args[4]), std::stoi(args[5]));
+
+  auto BC = sub.betweenness_centrality(std::stoi(args[6]));
+
+  /*
+  * Betweenness Centrality Output
+  */
+  std::cout << "---------------------------------------\n"; 
+  std::cout << "        Betweenness Centrality\n";
+  std::cout << "---------------------------------------\n"; 
+  for (unsigned i = 0; i < BC.size(); i++ ) {
+    auto pair = BC[i];
+    std::cout << "#" << i + 1 << ": ";
+    if (args[2] != "") {
+      std::cout << "Product: " << reader[pair.first] << " | "; 
+    }
+      std::cout << " Node: " << pair.first << " | Centrality: " << pair.second << std::endl;
+  }
+  std::cout << "\n";
+
+  sub.draw_graph(args[3], std::stoi(args[7]), std::stoi(args[8]), false);
+  std::string loc = args[3];
+  if (loc != "") {
+    loc = loc + "/";
+  }
+
+  /*
+  * Force-Directed Drawing Output
+  */
+  std::cout << "---------------------------------------\n"; 
+  std::cout << "         Force-Directed Drawing\n";
+  std::cout << "---------------------------------------\n";
+
+  std::cout << "Graph Drawn and stored at " << loc << "GraphDrawn_Size" << args[5] << ".svg\n" << std::endl;
+
+  /*
+  *  BFS Path Output
+  */
+  std::cout << "---------------------------------------\n"; 
+  std::cout << "              BFS Path\n";
+  std::cout << "---------------------------------------\n";
+
+  std::vector<int> path = graph.BFS(std::stoi(args[9]), std::stoi(args[10]));
+
+  for (unsigned i = 0; i < path.size(); i++) {
+    if (args[2] == "") {
+      std::cout << path[i];
+    } else {
+      std::cout << reader[path[i]] << " (" << path[i] << ") "; 
+    }
+    if (i < path.size() - 1) {
+      std::cout << " --> ";
+    }
+  }
+  std::cout << "\n\n";
 
   return 0;
 }
